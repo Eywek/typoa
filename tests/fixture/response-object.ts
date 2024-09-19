aa1d66489d582a40f62637fea8ad1f4e7a282bbf
Valentin Marchaud <thisismac47@gmail.com>
2024-09-19 17:49:09 +0200

    improv: support union/intersection in schema name (#19)

--- tests/fixture/response-object.ts
+++ tests/fixture/response-object.ts
@@ -5,6 +5,9 @@ type SuccessResponse<T extends any> = { data: T }
 type Entity = {
   name: string
 }
+type Entity2 = {
+  name: number
+}
 
 @Route('/my-3nd-controller/')
 @Security({ company: [] })
@@ -12,6 +15,14 @@ export class My3ndController {
   @Get('/')
   public async list (): Promise<SuccessResponse<Entity[]>> {
     return { data: [{ name: 'foo' }] }
+  }
+  @Get('/example')
+  public async getIntersection (): Promise<SuccessResponse<Entity & { example: string }>> {
+    return { data: { name: 'foo', example: '' } }
+  }
+  @Get('/union')
+  public async getUnion (): Promise<SuccessResponse<Entity| Entity2>> {
+    return { data: { name: 'foo' } }
   }
   @Post('/')
   public async create (): Promise<SuccessResponse<Entity>> {
